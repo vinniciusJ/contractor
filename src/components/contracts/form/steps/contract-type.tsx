9335080d9ef871a88e3bb440bdc38ac7b59e7308
Vinicius de Oliveira Jimenez <vinicius.jimenez@pti.org.br>
2024-09-01 16:20:23 -0300

    refactor: add array input to contract fortm

--- src/components/contracts/form/steps/contract-type.tsx
+++ src/components/contracts/form/steps/contract-type.tsx
@@ -1,24 +1,21 @@
-import { useCallback } from 'react'
-
-import { Add, TrashCan } from '@carbon/icons-react'
-import { Button, Grid, IconButton, Typography } from '@mui/material'
-import { useFieldArray, useFormContext } from 'react-hook-form'
+import { Grid, Typography } from '@mui/material'
+import { useFormContext } from 'react-hook-form'
 
 import { FormModal } from '@/components/ui/form-modal'
+import { ArrayInput } from '@/components/ui/inputs/array-input'
 import { DateInput } from '@/components/ui/inputs/date'
 import { Input } from '@/components/ui/inputs/input'
 import { Select } from '@/components/ui/inputs/select'
 import { ContractFormFields } from '@/schemas/contract'
+import { contractItemFormSchema } from '@/schemas/contract-item'
+import { optionsParser } from '@/schemas/utils/form'
+import { CONTRACT_TYPE_ITEM_TYPE_LABELS } from '@/utils/constants/labels'
+import { getSchemaDefault } from '@/utils/schema'
+
+const ITEMS_OPTIONS = optionsParser.parse(CONTRACT_TYPE_ITEM_TYPE_LABELS)
 
 export const ContractTypeStep = () => {
 	const { control } = useFormContext<ContractFormFields>()
-
-	const { fields, append, remove } = useFieldArray({
-		control,
-		name: 'contractItems',
-	})
-
-	const addContractItem = useCallback(() => append({ name: '', scheduledDate: '', type: 'DELIVERY' }), [append])
 
 	return (
 		<FormModal.StepContent step={1} columns={2}>
@@ -28,50 +25,33 @@ export const ContractTypeStep = () => {
 			<Grid item xs={1}>
 				<Input control={control} label="Objetivo do contrato" name="contractObjective" />
 			</Grid>
-			<Grid item xs={2}>
+			<Grid item xs={4}>
 				<Typography variant="h2" mb={1}>
 					Entregas
 				</Typography>
-				{fields.map((field, index) => (
-					<Grid container columns={13} key={field.id} marginBottom={2} direction="row" gap={1}>
-						<Grid item xs={6}>
-							<Input label="Nome" name={`contractItems.${index}.name`} control={control} />
-						</Grid>
-						<Grid item xs={3}>
-							<DateInput
-								label="Data Prevista"
-								name={`contractItems.${index}.scheduledDate`}
-								control={control}
-								visibleViews={['day', 'month', 'year']}
-							/>
-						</Grid>
-						<Grid item xs={3}>
-							<Select
-								name={`contractItems.${index}.type`}
-								options={[
-									{
-										label: 'Serviço',
-										value: 'SERVICE',
-									},
-									{
-										label: 'Entrega',
-										value: 'DELIVERY',
-									},
-								]}
-								label="Tipo"
-								control={control}
-							/>
-						</Grid>
-						<Grid item xs={1}>
-							<IconButton onClick={() => remove(index)}>
-								<TrashCan />
-							</IconButton>
-						</Grid>
-					</Grid>
-				))}
-				<Button sx={{ width: '100%' }} variant="outlined" onClick={addContractItem}>
-					<Add />
-				</Button>
+			</Grid>
+			<Grid item xs={4}>
+				<Grid item xs={4}>
+					<ArrayInput
+						control={control}
+						name="contractItems"
+						defaultValue={getSchemaDefault(contractItemFormSchema)}
+						renderInput={(name) => (
+							<>
+								<Input control={control} label="Nome" name={`${name}.name`} />
+
+								<Select control={control} label="Tipo" name={`${name}.type`} options={ITEMS_OPTIONS} />
+
+								<DateInput
+									control={control}
+									label="Data agendada"
+									name={`${name}.scheduledDate`}
+									visibleViews={['day', 'month', 'year']}
+								/>
+							</>
+						)}
+					/>
+				</Grid>
 			</Grid>
 		</FormModal.StepContent>
 	)
